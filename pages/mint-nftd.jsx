d09bd0a5d7f94da68b86c197651b5c24a3109019
karagyozovLT <georgi.karagyozov@looming.tech>
2023-03-08 00:09:33 +0200

    Update mint-nftd.jsx

--- pages/mint-nftd.jsx
+++ pages/mint-nftd.jsx
@@ -8,7 +8,7 @@ const MintNftd = () => {
         <Image
           width={100}
           height={10}
-          alt="BNFT Stablecoin logo"
+          alt="NFTD Stablecoin logo"
           src="/images/about/nftd-stablecoin-logo.png"
         />
       </div>
@@ -19,11 +19,13 @@ const MintNftd = () => {
             Mint NFTD with Virtual Land NFTs
           </h4>
           <p className="text-md mb-5">
-            Mint NFTD by selling Land NFTs in our virtual estate. Buyers
-            purchase Land NFTs using standard stablecoin, which is then used to
-            buy WETH on the open market. The WETH is staked, and the buyer
-            receives staking rewards from staked WETH. An equivalent amount of
-            NFTD is minted and sent to the seller`s wallet.
+            You can mint NFTD by selling Land NFTs within our virtual estate. To
+            purchase Land NFTs, buyers use standard stablecoin, which is then
+            utilized to purchase wETH on the open market. Once purchased, the
+            wETH is staked with ROCKET POOL. Our smart contract then receives
+            rETH, and starts sending staking rewards from the staked ETH to the
+            buyer of the Land NFT. At the same time, an equal amount of NFTD is
+            created and transferred to the seller`s wallet.
           </p>
         </div>
         <div className="mt-7 bg-[#eaf9ff] rounded-xl p-6">
@@ -31,12 +33,14 @@ const MintNftd = () => {
             Mint NFTD with Stablecoin
           </h4>
           <p className="text-md mb-5">
-            Mint NFTD with your standard stablecoin and start earning RETH
-            staking rewards simply by holding NFTD. Connect your wallet to our
-            minting site, select the desired stablecoin amount, and press the
-            MINT button to receive NFTD in your wallet. By minting NFTD, your
-            stablecoin is used to purchase WETH on the open market, which is
-            then staked, and the user receives staking rewards from staked WETH.
+            You can earn ETH staking rewards by simply holding NFTD, which can
+            be minted using your standard stablecoin. To mint NFTD, connect your
+            wallet to our minting site, select the desired stablecoin amount,
+            and press the MINT button to receive NFTD in your wallet. When you
+            mint NFTD, your standard stablecoin is utilized to purchase wETH on
+            the open market, which is then staked with ROCKET POOL. Our smart
+            contract receives rETH and starts sending staking rewards from
+            staked ETH to the user who minted the NFTD stablecoin.
           </p>
         </div>
         <div className="mt-7 bg-[#eaf9ff] rounded-xl p-6">
@@ -44,12 +48,14 @@ const MintNftd = () => {
             Mint NFTD with Cash
           </h4>
           <p className="text-md mb-5">
-            Connect your wallet to our fiat on-ramp and mint NFTD with your bank
-            card. Choose the desired stablecoin amount, press the MINT button,
-            and receive NFTD in your wallet. By minting NFTD, your card is
-            charged for the amount of stablecoin minted, and the cash is used to
-            buy WETH on the open market. The WETH is staked, and the user
-            receives staking rewards from staked WETH.
+            You can easily mint NFTD using your bank card by connecting your
+            wallet to our fiat onramp. To mint NFTD, choose the desired
+            stablecoin amount, press the MINT button, and receive NFTD in your
+            wallet. When you mint NFTD, your card is charged for the amount of
+            stablecoin minted, and the cash is utilized to purchase wETH on the
+            open market, which is then staked with ROCKET POOL. Our smart
+            contract receives rETH and starts sending staking rewards from
+            staked ETH to the user who minted the NFTD stablecoin.
           </p>
         </div>
       </div>
@@ -59,15 +65,16 @@ const MintNftd = () => {
         <p className="text-md mb-5">
           NFTD can be minted with Virtual Land NFT purchases, stablecoins, or
           cash/bank cards. Each user minting stablecoin is entitled to staking
-          rewards from staked WETH proportional to the amount of minted NFTD.
-          The protocol`s smart contract holds a liquid staking derivative called
-          RETH, which directs staking rewards only to the user. The LSD in our
-          smart contract can be redeemed only by the user who wants to burn the
-          stablecoin, and it is only released for redemption purposes. NFTD is
-          pegged to a value of 1 USD, maintained by similar algorithms as RAI
-          stablecoin. The redemption price is fixed at 1 USD, calculated by the
-          ratio of the market price of RETH and WETH. Market forces and
-          arbitrage incentives are in place to ensure the stability of the peg.
+          rewards from staked ETH proportional to the amount of minted NFTD. Our
+          protocol`s smart contract holds a liquid staking derivative of ROCKET
+          POOL Protocol called rETH, directing staking rewards only to the user.
+          The LSD in our smart contract can be redeemed only by the user who
+          wants to burn the stablecoin, and it is only released for redemption
+          purposes. NFTD is pegged to a value of 1 USD, maintained by similar
+          algorithms as RAI stablecoin. The redemption price is fixed at 1 USD,
+          calculated by the ratio of the market price of rETH and ETH. Market
+          forces and arbitrage incentives are in place to ensure the stability
+          of the peg.
         </p>
       </div>
     </div>
